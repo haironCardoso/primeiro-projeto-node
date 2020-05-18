39e6b9c3ba529a0cd919381b94b5d3bff78596d9
haironCardoso <hairon.cardoso@gmail.com>
2020-05-18 11:17:31 -0300

    Estrutura e padroes de codigo

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import routes from './routes/index';
+
+const app = express();
+app.use(express.json());
+app.use(routes);
+app.listen(3333, () => {
+    console.log('⚔ Server started port 3333');
+});
